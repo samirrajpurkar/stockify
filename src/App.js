fb50b0518ca6c31f2fc5ade0890dc6ce14dac1e0
samirrajpurkar <samir.rajpurkar@gmail.com>
2017-06-11 19:41:22 +0400

    Routing finally working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,162 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-console.log(BrowserRouter);
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import * as firebase from 'firebase';
 
-const App = () => (
-  <h1>Hello World</h1>
+const Links = () => (
+  <nav className="nav">
+    <Link className="nav-link" to="/">Home</Link>
+    <Link className="nav-link" to="/about">About</Link>
+    <Link className="nav-link" to="/contact">Contact</Link>
+  </nav>
 );
 
+class TodoList extends Component {
+  render() {
+    var _this = this;
+    var createItem = function(item, index) {
+      return (
+        <li key={ index }>
+          { item.text }
+          <span onClick={ _this.props.removeItem.bind(null, item['.key']) }
+                style={{ color: 'red', marginLeft: '10px', cursor: 'pointer' }}>
+            X
+          </span>
+        </li>
+      );
+    };
+    return <ul>{ this.props.items.map(createItem) }</ul>;
+  }
+}
+
+class App extends Component {
+    constructor() {
+    super();
+    this.state = {
+      items: [],
+      text: ''
+    }
+    // this.handleChange = this.handleChange.bind(this);
+    // this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    this.firebaseRef = firebase.database().ref('items');
+    this.firebaseRef.limitToLast(25).on('value', function(dataSnapshot) {
+      var items = [];
+      dataSnapshot.forEach(function(childSnapshot) {
+        var item = childSnapshot.val();
+        item['.key'] = childSnapshot.key;
+        items.push(item);
+      });
+
+      this.setState({
+        items: items
+      });
+    }.bind(this));
+  }
+
+  removeItem(key) {
+    var firebaseRef = firebase.database().ref('items');
+    firebaseRef.child(key).remove();
+  }
+
+  render() {
+    return (
+      <Router>
+       <div>
+         <Links />
+         <Route exact path="/" render={() => <TodoList 
+            items={this.state.items}
+            removeItem={this.removeItem}
+          />} />
+         <Route path="/about" render={() => <h1>About</h1>} />
+         <Route path="/contact" render={() => <h1>Contact</h1>} />
+       </div>
+     </Router>
+   );
+  }
+}
+
+// const App = () => (
+//   <Router>
+//     <div>
+//       <Links />
+//       <Route exact path="/" render={() => <h1>Home</h1>} />
+//       <Route path="/about" render={() => <h1>About</h1>} />
+//       <Route path="/contact" render={() => <h1>Contact</h1>} />
+//     </div>
+//   </Router>
+// );
+
+
+
+
+
+// class App extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       items: [],
+//       text: ''
+//     }
+
+//     this.handleChange = this.handleChange.bind(this);
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//   }
+
+//   componentDidMount() {
+//     this.firebaseRef = firebase.database().ref('items');
+//     this.firebaseRef.limitToLast(25).on('value', function(dataSnapshot) {
+//       var items = [];
+//       dataSnapshot.forEach(function(childSnapshot) {
+//         var item = childSnapshot.val();
+//         item['.key'] = childSnapshot.key;
+//         items.push(item);
+//       });
+
+//       this.setState({
+//         items: items
+//       });
+//     }.bind(this));
+//   }
+
+//   removeItem(key) {
+//     var firebaseRef = firebase.database().ref('items');
+//     firebaseRef.child(key).remove();
+//   }
+
+//   handleChange(event) {
+//       this.setState({text: event.target.value});
+//     }
+
+//   handleSubmit(event) {
+//     event.preventDefault();
+//     if (this.state.text && this.state.text.trim().length !== 0) {
+//       this.firebaseRef.push({
+//         text: this.state.text
+//       });
+//       this.setState({
+//         text: ''
+//       });
+//     }
+//     console.log(this.state.items);
+//   }
+
+//   render() {
+//     return (
+//           <div>
+//             <TodoList items={ this.state.items } removeItem={ this.removeItem } />
+//             <form onSubmit={ this.handleSubmit }>
+//               <input onChange={ this.handleChange } value={ this.state.text } />
+//               <button>{ 'Add #' + (this.state.items.length + 1) }</button>
+//             </form>
+//           </div>
+//         );
+//   }
+// }
+
 export default App;
+
+
+
--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import * as firebase from 'firebase';
+
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCXeCkVOdWN_MkwID2cHzxVV-miIl0sNRU",
+    authDomain: "stockifyfirebase.firebaseapp.com",
+    databaseURL: "https://stockifyfirebase.firebaseio.com",
+    projectId: "stockifyfirebase",
+    storageBucket: "stockifyfirebase.appspot.com",
+    messagingSenderId: "729840171264"
+  };
+  firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
